f025924f4af1111abf391e36bd3197c98bab1a5f
PaulNovrandes <novrandes.tanuma@gmail.com>
2023-05-25 08:19:42 +0700

    Tugas 24 JS
    Mengurutkan angka dengan sort. Tugas ini dikumpulkan kepada niomic.

---
+++ Tugas24.js
@@ -0,0 +1,10 @@
+function panggilSort(){
+  var array1 = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+  console.log("Sebelumnya : ",array1);
+  array1.sort();
+  console.log("Ascending : ",array1);
+  array1.reverse();
+  console.log("Descending : ",array1);
+}
+
+panggilSort();
